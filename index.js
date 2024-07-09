ac7afff0857fb330c561e4c850e2e3630c144190
trex2004 <98643120+trex2004@users.noreply.github.com>
2024-07-09 21:28:36 +0530

    Update index.js
    app.use(cors({
        origin:["https://expense-manger-frontend.vercel.app/"],
        methods:["POST","GET"],
        credentials: true
    }))

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ const PORT = process.env.PORT || 8000
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(cors({
+    origin:["https://expense-manger-frontend.vercel.app/"],
+    methods:["POST","GET"],
+    credentials: true
+}))
 
 //routes
 app.use('/api/v1/users',userRouter)
